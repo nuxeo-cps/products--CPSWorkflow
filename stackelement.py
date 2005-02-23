2fcb85c8a73ce423273a497a153356c9d6e35408
janguenot <devnull@localhost>
2005-02-23 15:11:21 +0000

    View / Edit guards fixtures on stack elements.

--- stackelement.py
+++ stackelement.py
@@ -25,8 +25,11 @@ A Stack ELement is an element stored within a stack type.
 from types import StringType
 
 from Globals import InitializeClass
-from AccessControl import ClassSecurityInfo
+from AccessControl import ClassSecurityInfo, getSecurityManager
+from Acquisition import aq_parent, aq_inner
 from OFS.SimpleItem import SimpleItem
+
+from Products.CMFCore.utils import getToolByName
 
 from stackdefinitionguard import StackDefinitionGuard as Guard
 
@@ -45,11 +48,18 @@ class StackElement(SimpleItem):
     meta_type = 'Stack Element'
     prefix = ''
 
+    hidden_meta_type = ''
+
     __implements__ = (IStackElement,)
 
     security = ClassSecurityInfo()
 
-    guard = None
+    view_guard = None
+    edit_guaed = None
+
+    #
+    # PRIVATE
+    #
 
     def __cmp__(self, other):
         if isinstance(other, StackElement):
@@ -64,6 +74,10 @@ class StackElement(SimpleItem):
     def __str__(self):
         raise NotImplementedError
 
+    #
+    # API
+    #
+
     def getPrefix(self):
         """Returns the prefix for this stack element
         """
@@ -74,20 +88,107 @@ class StackElement(SimpleItem):
         """
         return self()[len(self.getPrefix())+1:]
 
-    def getGuard(self):
-        """Return a temporarly guard instance
-        """
-        if self.guard is not None:
-            return self.guard
-        else:
-            return Guard().__of__(self)
+    def getHiddenMetaType(self):
+        return self.hidden_meta_type
 
-    def getGuardSummary(self):
-        """Return a guard summary for display purpose
+    #
+    # SECURITY
+    #
+
+    def isVisible(self, sm, stack, context):
+        """Is the entry visible by the user
+
+        returns a boolean
         """
-        res = None
-        if self.guard is not None:
-            res = self.guard.getSummary()
-        return res
+
+        # Standalone elt
+        if stack is None or context is None:
+            return 1
+
+        wftool = getToolByName(context, 'portal_workflow', None)
+        if wftool is None:
+            # No workflow tool thus standalone
+            return 1
+
+        # XXX CPS assumptions
+        wf_def = wftool.getWorkflowsFor(context)[0]
+
+        # First check if there's an override
+        if self.getViewGuard():
+            return self.getViewGuard().check(sm, wf_def, context)
+
+        # Evaluate the default guard for view of the stackdef within
+        # the stack context
+        else:
+            for k, v in wftool.getStacks(context).items():
+                if v == stack:
+                    stackdef = wftool.getStackDefinitionFor(context, k)
+                    guard = stackdef.getViewStackElementGuard()
+                    return guard.check(sm, wf_def, context)
+        return 1
+
+    def isEditable(self, sm, stack, context):
+        """Is the entry editable by the user
+
+        returns a boolean
+        """
+
+        # Standalone elt
+        if stack is None or context is None:
+            return 1
+
+        wftool = getToolByName(context, 'portal_workflow', None)
+        if wftool is None:
+            # No workflow tool thus standalone
+            return 1
+
+        # XXX CPS assumptions
+        wf_def = wftool.getWorkflowsFor(context)[0]
+
+        # First check if there's an override
+        if self.getViewGuard():
+            return self.getViewGuard().check(sm, wf_def, context)
+
+        # Evaluate the default guard for view of the stackdef within
+        # the stack context
+        else:
+            for k, v in wftool.getStacks(context).items():
+                if v == stack:
+                    stackdef = wftool.getStackDefinitionFor(context, k)
+                    guard = stackdef.getEditStackElementGuard()
+                    return guard.check(sm, wf_def, context)
+        return 1
+
+
+    #
+    # GUARDS
+    #
+
+    def getViewGuard(self):
+        return self.view_guard
+
+    def setViewGuard(self, guard_permissions='', guard_roles='',
+                     guard_groups='', guard_expr=''):
+        self.view_guard = Guard()
+        _props = {'guard_permissions':guard_permissions,
+                  'guard_roles':guard_roles,
+                  'guard_groups':guard_groups,
+                  'guard_expr':guard_expr,
+                  }
+        self.getViewGuard().changeFromProperties(_props)
+
+    def getEditGuard(self):
+        return self.edit_guard
+
+    def setEditGuard(self, guard_permissions='', guard_roles='',
+                     guard_groups='', guard_expr=''):
+        self.edit_guard = Guard()
+        _props = {'guard_permissions':guard_permissions,
+                  'guard_roles':guard_roles,
+                  'guard_groups':guard_groups,
+                  'guard_expr':guard_expr,
+                  }
+        self.getEditGuard().changeFromProperties(_props)
+
 
 InitializeClass(StackElement)
