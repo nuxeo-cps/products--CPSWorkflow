71924a09ca104fdd0d7f4d04140b69ab70456920
Georges Racinet on purity.racinet.fr <georges@racinet.fr>
2010-10-02 20:27:32 +0200

    #2179: did not know about the aggregated wf histories in repo tool

--- upgrade.py
+++ upgrade.py
@@ -28,14 +28,22 @@ from Products.CPSCore.interfaces import ICPSProxy
 
 logger = logging.getLogger(__name__)
 
+def upgrade_transitions_unicode(transitions):
+    """Upgrade any list of transition statuses."""
+    for trans in transitions:
+        c = trans.get('comments')
+        if c is not None:
+            trans['comments'] = upgrade_string_unicode(c)
+
+
 def upgrade_unicode_for(proxy):
     """Upgrade workflow history for proxy."""
 
-    for transitions in proxy.workflow_history.values():
-        for trans in transitions:
-            c = trans.get('comments')
-            if c is not None:
-                trans['comments'] = upgrade_string_unicode(c)
+    wfh = proxy.workflow_history
+    for transitions in wfh.values():
+        upgrade_transitions_unicode(transitions)
+
+    wfh._p_changed = 1 # PersistentMapping cannot guess in deeper structure
     return True
 
 def upgrade_unicode_in(folder, counters=None, walk_folders=False):
@@ -65,11 +73,38 @@ def upgrade_unicode_in(folder, counters=None, walk_folders=False):
 def upgrade_unicode(portal):
     cpounters = dict(done=0, total=0)
 
-    logger.info("Starting workflow history upgrade for all documents.")
+    logger.info("Starting workflow history upgrade for proxies.")
     counters = dict(done=0, total=0)
-    upgrade_unicode_in(portal, counters=counters, walk_folders=True)
+    #upgrade_unicode_in(portal, counters=counters, walk_folders=True)
     transaction.commit()
 
     logger.warn(
         "Finished to upgrade workflow history. Successful for %d/%d proxies",
         counters['done'], counters['total'])
+    upgrade_aggregated_histories(portal)
+
+def upgrade_aggregated_histories(portal):
+    repotool = portal.portal_repository
+    logger.info("Starting aggregated workflow history upgrade in repository")
+    histories = repotool._histories
+
+    done = 0
+    docids = repotool.listDocids() # not efficient, but avoids side-effects
+    total = len(docids)
+
+    for docid in docids:
+        # implementation as get/set because changing inside such a simple
+        # tuple is not persistable.
+        # If it were PersistentList, things would be better.
+        transitions = repotool.getHistory(docid)
+        if transitions is None:
+            continue
+        upgrade_transitions_unicode(transitions)
+        repotool.setHistory(docid, transitions)
+
+        done += 1
+        if done % 1000 == 0:
+            logger.info("Upgraded %d/%d aggregated workflow histories",
+                        done, total)
+            transaction.commit()
+
